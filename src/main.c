fd9e619eb85e04a5ca9bfc73ce544575558408de
Iaroslav Erokhin <ysoftware@yandex.ru>
2024-08-26 08:39:50 +0200

    prettify code

--- src/main.c
+++ src/main.c
@@ -13,10 +13,7 @@
 #include <sys/stat.h>
 #include <time.h>
 
-#define DEBUG 0
-#define PROJECT_NAME "C24MobileSimOnly"
-
-void find_latest_file(const char *directory, char *latest_file, time_t *latest_mtime) {
+void find_latest_file(const char *directory, char *latest_file, time_t *latest_mtime, const char *project_name) {
     DIR *dir = opendir(directory);
     if (!dir) {
         fprintf(stderr, "Unable to access directory: %s\n", directory);
@@ -29,19 +26,20 @@ void find_latest_file(const char *directory, char *latest_file, time_t *latest_m
             continue;
         }
 
-        // this is how we skip all other folders
-        if (strstr(directory, PROJECT_NAME) == NULL) {
-            // when PROJECT_PATH is not included in the dir, this means we didn't open it yet - keep looking
-            if (strstr(entry->d_name, PROJECT_NAME) == NULL) {
-                continue;
-            }
-        } else if (strstr(directory, "Logs") == NULL) {
-            if (strstr(entry->d_name, "Logs") == NULL) {
-                continue;
-            }
-        } else if (strstr(directory, "Build") == NULL) {
-            if (strstr(entry->d_name, "Build") == NULL) {
-                continue;
+        // this is how we find our folder
+        if (project_name != NULL) {
+            if (strstr(directory, project_name) == NULL) {
+                if (strstr(entry->d_name, project_name) == NULL) {
+                    continue;
+                }
+            } else if (strstr(directory, "Logs") == NULL) {
+                if (strstr(entry->d_name, "Logs") == NULL) {
+                    continue;
+                }
+            } else if (strstr(directory, "Build") == NULL) {
+                if (strstr(entry->d_name, "Build") == NULL) {
+                    continue;
+                }
             }
         }
 
@@ -55,7 +53,7 @@ void find_latest_file(const char *directory, char *latest_file, time_t *latest_m
         }
 
         if (S_ISDIR(file_stat.st_mode)) {
-            find_latest_file(path, latest_file, latest_mtime);
+            find_latest_file(path, latest_file, latest_mtime, project_name);
         } else if (S_ISREG(file_stat.st_mode) && strstr(entry->d_name, ".xcactivitylog") != NULL) {
             if (file_stat.st_mtime > *latest_mtime) {
                 *latest_mtime = file_stat.st_mtime;
@@ -67,7 +65,7 @@ void find_latest_file(const char *directory, char *latest_file, time_t *latest_m
     closedir(dir);
 }
 
-void parse_xcactivitylog(char *input, FILE *output) {
+void parse_xcactivitylog(char *input, FILE *output, bool output_full_log) {
     const char *p = input;
 
     if (strncmp(p, "SLF0", 4) != 0) {
@@ -89,73 +87,57 @@ void parse_xcactivitylog(char *input, FILE *output) {
     int column;
 
     int classes_found = 1;
-    int next_padded_instances = 0; // to print arrays
     char *classes[10];
 
     while (*p != '\0') {
         if (*p >= '0' && *p <= '9' || *p >= 'a' && *p <= 'f') {
             uint64_t value = 0;
-            uint64_t hex_value = 0;
+            // to not fail when encountering doubles, we also parse these
             while ((*p >= '0' && *p <= '9') || (*p >= 'a' && *p <= 'f') || (*p >= 'A' && *p <= 'F')) {
                 value = value * 10 + (*p - '0');
-                /* hex_value = hex_value * 16 + (*p >= 'a' ? *p - 'a' + 10 : (*p >= 'A' ? *p - 'A' + 10 : *p - '0')); // this is not correct */
                 (p)++;
             }
 
             if (*p == '#') { // integer
-#if DEBUG
-                if (next_padded_instances > 0) { fprintf(output, "    "); }
-                fprintf(output, "[type: \"int\", value: %" PRIu64 "]\n", value);
-#endif
+                if (output_full_log) {
+                    fprintf(output, "[type: \"int\", value: %" PRIu64 "]\n", value);
+                }
 
                 if (next_int_is_line) {
                     next_int_is_line = false;
                     next_int_is_column = true;
                     line = value;
-                    /* printf("found line\n"); */
                 } else if (next_int_is_column) {
                     next_int_is_column = false;
                     column = value;
-                    /* printf("found column\n"); */
                 }
 
                 p++;
             } else if (*p == '^') { // double
-                double double_value = *((double*)&hex_value);
-                double_value = __builtin_bswap64(hex_value);  // Convert from little-endian
-#if DEBUG
-                if (next_padded_instances > 0) { fprintf(output, "    "); }
-                fprintf(output, "[type: \"double\", value: %f]\n", double_value);
-#endif
+                if (output_full_log) {
+                    fprintf(output, "[type: \"double\", value: not parsed]\n");
+                }
                 p++;
-            } else if (*p == '@') { // classInstance with index of declared class
+            } else if (*p == '@') { // classInstance
                 char *class_name = classes[value];
-            
+
                 if (strcmp(class_name, "IDEDiagnosticActivityLogMessage") == 0) {
-                    if (found_diagnostic_activity_log_message) {
-                        /* printf("WTF! WE ARE ALREADY LOOKING AT ONE\n"); */
-                    }
-                    /* printf("\n>>> IDEDiagnosticActivityLogMessage\n"); */
                     found_diagnostic_activity_log_message = true;
                     next_string_is_message = true;
                     next_string_is_log_type = false;
                     next_string_is_file = false;
                 } else if (found_diagnostic_activity_log_message && strcmp(class_name, "DVTTextDocumentLocation") == 0) {
-                    /* printf(" - Found DVTTextDocumentLocation following IDEDiagnosticActivityLogMessage\n"); */
                     next_string_is_file = true;
                     next_int_is_line = true;
                 } else {
-                    /* printf("found something else..... '%s'\n", class_name); */
                     found_diagnostic_activity_log_message = false;
                     next_string_is_file = false;
                     next_int_is_line = false;
                 }
 
-#if DEBUG
-                if (next_padded_instances > 0) { fprintf(output, "    "); }
-                fprintf(output, "[type: \"classInstance\", value: \"%s\"]\n", class_name);
-#endif
-                next_padded_instances--;
+                if (output_full_log) {
+                    fprintf(output, "[type: \"classInstance\", value: \"%s\"]\n", class_name);
+                }
                 p++;
             } else if (*p == '"' || *p == '*') { // string
                 p++;
@@ -167,10 +149,8 @@ void parse_xcactivitylog(char *input, FILE *output) {
                     if (next_string_is_message) {
                         message = str_value;
                         next_string_is_message = false;
-                        /* printf(" - Found Message: %s\n", message); */
                     } else if (next_string_is_file) {
                         file_name = str_value;
-                        /* printf(" - File name: %s\n", file_name); */
                         next_string_is_file = false;
                         next_string_is_log_type = true;
                     } else if (next_string_is_log_type) {
@@ -178,10 +158,7 @@ void parse_xcactivitylog(char *input, FILE *output) {
 
                         if (strcmp(str_value, "Swift Compiler Error") == 0) {
                             printf("%s:%d:%d: %s\n", file_name, line + 1, column + 1, message);
-                            /* printf("%s:%d:%d: %s\n", file_name, line + 1, column + 1, message); */
-                            /* printf("Found swift compiler error\n\n"); */
                         } else {
-                            /* printf(" - Nope! the string was '%s'\n\n", str_value); */
                             found_diagnostic_activity_log_message = false;
                         }
                     }
@@ -189,10 +166,9 @@ void parse_xcactivitylog(char *input, FILE *output) {
 
                 // free(str_value); // who cares
 
-#if DEBUG
-                if (next_padded_instances > 0) { fprintf(output, "    "); }
-                fprintf(output, "[type: \"string\", length: %llu, value: \"%s\"]\n", value, str_value);
-#endif
+                if (output_full_log) {
+                    fprintf(output, "[type: \"string\", length: %llu, value: \"%s\"]\n", value, str_value);
+                }
                 p += value;
             } else if (*p == '%') { // className
                 p++;
@@ -200,28 +176,26 @@ void parse_xcactivitylog(char *input, FILE *output) {
                 strncpy(str_value, p, value);
                 str_value[value] = '\0';
                 p += value;
-#if DEBUG
-                fprintf(output, "[type: \"className\", index: %d, value: \"%s\"]\n", classes_found, str_value);
-#endif
+                if (output_full_log) {
+                    fprintf(output, "[type: \"className\", index: %d, value: \"%s\"]\n", classes_found, str_value);
+                }
                 classes[classes_found] = str_value;
                 classes_found++;
 
                 // free(str_value); // leak all class names, because we use them later
             } else if (*p == '(') {
-                next_padded_instances = value;
-#if DEBUG
-                fprintf(output, "[type: \"array\", count: %" PRIu64 "]\n", value);
-#endif
+                if (output_full_log) {
+                    fprintf(output, "[type: \"array\", count: %" PRIu64 "]\n", value);
+                }
                 p++;
             } else { // unexpected
                 fprintf(stderr, "[ERROR] Unexpected token %c after a number.\n", *p);
                 exit(1);
             }
         } else if (*p == '-') {
-#if DEBUG
-            if (next_padded_instances > 0) { fprintf(output, "    "); }
-            fprintf(output, "[type: \"null\"]\n");
-#endif
+            if (output_full_log) {
+                fprintf(output, "[type: \"null\"]\n");
+            }
             p++;
         } else if (*p == '\n') {
             p++;
@@ -235,13 +209,27 @@ void parse_xcactivitylog(char *input, FILE *output) {
 }
 
 int main(int argc, char *argv[]) {
+    if (argc < 2 || strcmp(argv[1], "-h") == 0) {
+        printf("Usage: %s <project_name> [-d]\n\n", argv[0]);
+        printf("Latest modified found inside will by analysed (~/Library/Developer/Xcode/DerivedData/[project_name]*/Logs/Build/)\n\n");
+        printf("Pass flag -d to dump the whole parsed log as log-dump.txt\n");
+        exit(1);
+    }
+
+    bool output_full_log = false;
+    if (argc >= 3 && strcmp(argv[2], "-d") == 0) {
+        output_full_log = true;
+    }
+
     char path[250];
     const char *home_dir = getenv("HOME");
     sprintf(path, "%s/Library/Developer/Xcode/DerivedData", home_dir);
 
+    char *project_name = argv[1];
+
     char latest_file[1024] = {0};
     time_t latest_mtime = 0;
-    find_latest_file(path, latest_file, &latest_mtime);
+    find_latest_file(path, latest_file, &latest_mtime, project_name);
     if (strlen(latest_file) > 0) {
     } else {
         printf("No .xcactivitylog files found.\n");
@@ -253,32 +241,25 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    FILE *output;
-#if DEBUG
-    output = fopen("log-dump.txt", "w");
-    if (!output) {
-        perror("Failed to open output file");
-        gzclose(gz_input);
-        return 1;
+    FILE *output = NULL;
+    if (output_full_log) {
+        output = fopen("log-dump.txt", "w");
+        if (!output) {
+            perror("Failed to open output file");
+            gzclose(gz_input);
+            return 1;
+        }
     }
-#endif
 
-    /* FILE *output_log = fopen("error.log", "w"); */
-    /* if (!output_log) { */
-    /*     perror("Failed to open error log file"); */
-    /*     gzclose(gz_input); */
-    /*     return 1; */
-    /* } */
-
-    const int BUFFER_SIZE = 100000000; // 100 MB cuz M2 is a supercomputer
+    const int BUFFER_SIZE = 100000000; // 100 MB is easy for modern computers
     char *buffer = (char*) malloc(BUFFER_SIZE);
     gzread(gz_input, buffer, BUFFER_SIZE);
-    parse_xcactivitylog(buffer, output);
+    parse_xcactivitylog(buffer, output, output_full_log);
 
     gzclose(gz_input);
-#if DEBUG
-    fclose(output);
-#endif
+    if (output_full_log) {
+        fclose(output);
+    }
 
     return 0;
 }
